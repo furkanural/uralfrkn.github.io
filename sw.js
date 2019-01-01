1df2c05a9005417e4c3d2bbd7a24234cf130da5d
Furkan Ural <uralfrkn@gmail.com>
2019-01-01 04:32:29 +0300

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fd6e031d26f5a4741d0c.js"
+    "url": "webpack-runtime-37e9b718308d5bf8a642.js"
   },
   {
     "url": "app-02d73d454a526f853d24.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8fada38b6a49d3a8b3ea369e13909e83"
+    "revision": "cfeac758df5919137dd106ffd70574f4"
   },
   {
     "url": "0-14745af6e5bed7778187.js"
